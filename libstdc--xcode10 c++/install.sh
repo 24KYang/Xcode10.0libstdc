9759bead5710f66b906d60743c799439803b56ab
四五20 <916391479@qq.com>
2019-06-12 00:17:00 +0800

    add

---
+++ libstdc--xcode10 c++/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Asking sudo
+if [ $EUID != 0 ]; then
+    sudo "$0" "$@"
+    exit $?
+fi
+
+sudo cp /Users/yang/Downloads/libstdc--xcode10 c++/1/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/Library/CoreSimulator/Profiles/Runtimes/iOS.simruntime/Contents/Resources/RuntimeRoot/usr/lib/
+sudo cp /Users/yang/Downloads/libstdc--xcode10 c++/2/* /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/lib/
+sudo cp /Users/yang/Downloads/libstdc--xcode10 c++/3/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/usr/lib/
+sudo cp /Users/yang/Downloads/libstdc--xcode10 c++/4/* /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk/usr/lib/
+
+
+
+
+/Users/yang/Downloads/libstdc--xcode10 c++  这个路径只是一个例子
